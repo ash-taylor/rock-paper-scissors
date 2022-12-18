25e70ab6489943f18750f166107de4fecd0051bb
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-18 22:30:15 +0000

    Refactored JS, added HTML & CSS to build a working web-based UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,30 +1,61 @@
 let playerScore = 0;
 let computerScore = 0;
 let gamesPlayed = 0;
+let computerChoice;
 
+let win = (player, computer) => round.textContent = `Round ${gamesPlayed}: You Win! ${player} beats ${computer}`;
+let loss = (computer, player) => round.textContent = `Round ${gamesPlayed}: You Lose! ${computer} beats ${player}`;
+let draw = (computer) => round.textContent = `Round ${gamesPlayed}: It's a draw, you both chose ${computer}`;
+
+const buttons = document.querySelectorAll('.gameButton');
+let resultsBox = document.querySelector('.resultsBox');
+const gameHistory = document.querySelector('.gameHistory');
+let currentScore = document.querySelector('.currentScore');
+const gameResult = document.createElement('h2');
+const gameButton = document.createElement('button');
+const score = document.createElement('h3');
+currentScore.appendChild(score);
+const round = document.createElement('h3');
+gameHistory.appendChild(round);
+gameButton.classList.add('gameButton');
+gameButton.textContent = "New Game!";
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        gamesPlayed += 1;
+        if (gamesPlayed < 6) {
+            computerChoice = getComputerChoice();
+            playRound(button.id, computerChoice);
+            score.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+            if (gamesPlayed === 5) game();
+        }
+        else {
+            alert("Start a New Game!")
+            return;
+        }
+    }) 
+})
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 10) % 3;
-
     switch(randomNumber) {
         case 0:
-            return 'rock';
+            return 'Rock';
         case 1:
-            return 'paper';
+            return 'Paper';
         case 2:
-            return 'scissors';
+            return 'Scissors';
     }
 }
-
 
 function playRound(player, computerSelection) {
     if (player === computerSelection) {
         playerScore += 1;
         computerScore += 1;
-        return `Draw! You both chose ${player}`;
-    } else if ((player === "rock" && computerSelection === "scissors") || (
-    player === "scissors" && computerSelection === "paper") || (
-    player === "paper" && computerSelection === "rock")) {
+        return draw(computerSelection);
+    } else if ((player === "Rock" && computerSelection === "Scissors") || (
+    player === "Scissors" && computerSelection === "Paper") || (
+    player === "Paper" && computerSelection === "Rock")) {
         playerScore += 1;
         return win(player, computerSelection);
     } else {
@@ -33,51 +64,31 @@ function playRound(player, computerSelection) {
     }   
 }
 
-let win = (player, computer) => `You Win! ${player} beats ${computer}`;
-let loss = (computer, player) => `You Lose! ${computer} beats ${player}`;
-
-
-const buttons = document.querySelectorAll('button');
-
-let results = document.querySelector('.currentScore');
-const score = document.createElement('p');
-score.textContent = playerScore.toString();
-results.appendChild(score);
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(button.id);
-        if (gamesPlayed < 5) {
-            playRound(button.id, getComputerChoice());
-        }
-        else {
-            alert("Start a New Round!")
-        }
-        score.textContent = playerScore.toString();
-        results.appendChild(score);
-        gamesPlayed += 1;
-        if (gamesPlayed === 5) game();
-    }) 
-})
-
-let result = document.querySelector('.result');
-const gameResult = document.createElement('p');
-
 function game() {
     if (playerScore > computerScore) {
-        gameResult.textContent = "You Win!";
+        gameResult.textContent = "GAME OVER - YOU WIN!";
     }
-    else if (computerScore < playerScore) {
-        gameResult.textContent = "You You Lose!";
+    else if (computerScore > playerScore) {
+        gameResult.textContent = "GAME OVER - YOU LOSE!";
     }
     else {
-        gameResult.textContent = "It's a Draw!";
+        gameResult.textContent = "GAME OVER - IT'S A DRAW!";
     }
-    result.appendChild(gameResult);
+    resultsBox.appendChild(gameResult);
+    resultsBox.appendChild(gameButton);
+
+    gameButton.addEventListener('click', () => {
+        newGame();
+        currentScore.removeChild(score);
+        resultsBox.removeChild(gameResult);
+        resultsBox.removeChild(gameButton);
+    })
 }
 
 function newGame() {
     playerScore = 0;
     computerScore = 0;
     gamesPlayed = 0;
+    round.textContent = "";
+    score.textContent = "";
 }
